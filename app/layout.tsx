e6ca5c674f778d68fbefda7d639ea8a540578120
DaneshH096 <103094290+DaneshH096@users.noreply.github.com>
2025-03-30 15:22:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'All in one Calculator',
+  description: 'Created buy Danesh Hosur',
   generator: 'v0.dev',
 }
 
